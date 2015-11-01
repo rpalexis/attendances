86a426edb4cef890328ab021e4ff42a3e50a3841
Rulx Philome Alexis <rulxphilome.alexis@gmail.com>
2015-11-01 03:48:15 -0500

    Feat: First admin configuration

--- structure/admin.py
+++ structure/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from structure.models import *
 
 # Register your models here.
+
+class DomainAdmin(admin.ModelAdmin):
+	pass
+
+
+class NiveauAdmin(admin.ModelAdmin):
+	pass
+
+
+class AnneeacademiqueAdmin(admin.ModelAdmin):
+	pass
+
+
+class PromotionAdmin(admin.ModelAdmin):
+	pass
+
+
+class EtudiantAdmin(admin.ModelAdmin):
+	pass
+
+
+class ProfesseurAdmin(admin.ModelAdmin):
+	pass
+
+
+class MatiereAdmin(admin.ModelAdmin):
+	pass
+
+
+class HoraireAdmin(admin.ModelAdmin):
+	pass
+
+
+class SalleAdmin(admin.ModelAdmin):
+	pass
+
+
+admin.site.register(Domaine,DomainAdmin)
+admin.site.register(Niveau,NiveauAdmin)
+admin.site.register(Anneeacademique,AnneeacademiqueAdmin)
+admin.site.register(Promotion,PromotionAdmin)
+admin.site.register(Etudiant,EtudiantAdmin)
+admin.site.register(Professeur,ProfesseurAdmin)
+admin.site.register(Matiere,MatiereAdmin)
+admin.site.register(Horaire,HoraireAdmin)
+admin.site.register(Salle,SalleAdmin)
