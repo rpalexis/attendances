d779907c23523b281611db928b1df4cb8c4852ae
Rulx Philome Alexis <neeugene@inform5.capitalbankhaiti.ht>
2015-11-01 01:36:57 -0400

    Initial commit | Project is in hibeginning

---
+++ attendanceESIH/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for attendanceESIH project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "attendanceESIH.settings")
+
+application = get_wsgi_application()
