dcc7ecf08386b2ab3c2859362059c874e67d438c
Rulx Philome Alexis <rulxphilome.alexis@gmail.com>
2015-11-11 10:54:22 -0500

    testing Grappelli

--- attendanceESIH/settings.py
+++ attendanceESIH/settings.py
@@ -31,10 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django.contrib.contenttypes',
+    'grappelli.dashboard',
     'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -105,3 +106,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = (
         os.path.join(BASE_DIR, 'static')
     )
+
+GRAPPELLI_INDEX_DASHBOARD = "dashboard.CustomIndexDashboard"
